acdd442e4ccbea3c8e6dc7eaea1520ad9feb3e45
IamMrHenry <110964326+IamMrHenry@users.noreply.github.com>
2024-05-28 18:33:35 -0300

    Update app.py

--- PBL3/app.py
+++ PBL3/app.py
@@ -1,7 +1,7 @@
-# app.py
-from controllers.app_controller import create_app
-from utils.create_db import create_db
+from controladores.app_controlador import create_app
+from utils.criar_db import criar_db
 
 if __name__ == "__main__":
     app = create_app()
+    criar_db(app)
     app.run(host='0.0.0.0', port=8080, debug=True)
